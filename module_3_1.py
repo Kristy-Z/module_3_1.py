a7a34436516fd3e07ebbe66ff753641e1deaadc0
Kristy-Z <kristy38rus@gmail.com>
2024-08-30 11:26:35 +0700

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -8,21 +8,22 @@ count_calls()
 
 string = 'capybara'
 def string_info(string):
-    global calls
-    calls += 1
-    print(len(string), string.upper(), string.lower())
+    a = len(string), string.upper(), string.lower()
+    print(a)
 
 string_info(string)
+count_calls()
 
 string = 'armageddon'
 list_to_search = [1, 2, 3]
-def is_contains():
-    global calls
-    calls += 1
-    print(len(string), string.upper(), string.lower())
-    calls += 1
+def is_contains(string, list_to_search):
+    b = len(string), string.upper(), string.lower()
+    print(b)
+    count_calls()
+
     print('armageddon' in string)
     print('armageddon' in list_to_search)
-    print(calls)
 
-is_contains()
+is_contains(string, list_to_search)
+count_calls()
+print(calls)
